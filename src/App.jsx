55b477e5da885a79f3611a7900d72629e0dac26b
Tanrrr <tanrrrbronson@gmail.com>
2026-08-27 17:42:55 -0600

    chore: remove Windows-7-desktop concept and its dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-import './App.css';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Skills from './pages/Skills';
-import Resume from './pages/Resume';
-import Sidebar from './components/Sidebar';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-
-function App() {
-  return (
-    <div className="flex bg-bgimg bg-cover bg-repeat-y animate-slide-down">
-    <BrowserRouter>
-      <div class=" sticky transition-width max-sm:pb-16 sm:pl-20"><Sidebar/></div>
-      
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/resume" element={<Resume />} />
-      </Routes>
-    </BrowserRouter>
-    </div>
-  );
+export default function App() {
+  return <div />;
 }
-
-export default App;
